d357942a2dd1645ebc7b82a7eb40d26a55afac7b
Alan Hadzaragov <alan.hadzaragov@saltpepper.ru>
2017-10-14 09:43:36 +0300

    Add GET /todos/:id. Add tests. Set up heroku

--- server/server.js
+++ server/server.js
@@ -4,7 +4,9 @@ const bodyParser = require('body-parser');
 const mongoose = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.json());
@@ -27,10 +29,27 @@ app.get('/todos', (req, res) => {
   }, e => {
     res.status(400).send(e);
   })
-})
+});
 
-app.listen(3000, () => {
-  console.log('Started at port 3000');
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then(todo => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch(e => {
+    res.status(404).send();
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Started at port ${port}`);
 });
 
 module.exports = {
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,12 +3,15 @@ const request = require('supertest');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {ObjectID} = require('mongodb');
 
 const todos = [
   {
+    _id: new ObjectID(),
     text: 'First test todo'
   },
   {
+    _id: new ObjectID(),
     text: 'Second test todo'
   }
 ];
@@ -73,6 +76,33 @@ describe('GET /todos', () => {
       .expect(res => {
         expect(res.body.todos.length).toBe(2)
       })
+      .end(done);
+  });
+});
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', done => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo.text).toBe(todos[0].text)
+      })
+      .end(done);
+  });
+
+  it('it should return 404 if todo not found', done => {
+    const hexId = new ObjectID().toHexString();
+    request(app)
+      .get(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for invalid ids', done => {
+    request(app)
+      .get(`/todos/invalid_id`)
+      .expect(404)
       .end(done);
   });
 });
